5542acf322f1f7b76ae9ae8f1e9afb1b0f645d65
nidup <duponn@gmail.com>
2024-10-05 18:02:56 +0200

    music

--- planet.gd
+++ planet.gd
@@ -3,6 +3,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	_generate_friskies()
+	# degueu mais marche
+	$MusicPlayerStep1.play()
+	#$MusicPlayerStep2.play()
+	#$MusicPlayerStep2.stop()
+	#$MusicPlayerStep3.play()
+	#$MusicPlayerStep3.stop()
 
 func _generate_friskies() -> void:
 	var frisky_model = preload("res://frisky.tscn")	
@@ -14,6 +20,25 @@ func _generate_friskies() -> void:
 		new_frisky.visible = true
 		self.add_child(new_frisky)
 
+func _get_level() -> int:
+	if $CharacterBody2D.FUEL > 10:
+		return 3
+	elif $CharacterBody2D.FUEL > 5:
+		return 2
+	else:
+		return 1
+
 func _process(delta):
-	if $MusicPlayer.playing == false:
-		$MusicPlayer.play()
+	var level = _get_level()
+	if level == 2 and $MusicPlayerStep2.playing == false:
+		$MusicPlayerStep1.stop()
+		$MusicPlayerStep2.play()
+		$MusicPlayerStep3.stop()
+	elif level == 3 and $MusicPlayerStep3.playing == false:
+		$MusicPlayerStep1.stop()
+		$MusicPlayerStep2.stop()
+		$MusicPlayerStep3.play()
+	elif level == 1 and $MusicPlayerStep1.playing == false:
+		$MusicPlayerStep1.play()
+		$MusicPlayerStep2.stop()
+		$MusicPlayerStep3.stop()
