e074c151d83573ae2e4956ab1a696fb3e3d78c40
Pierre Allard <pierallard@gmail.com>
2024-10-07 11:26:17 +0200

    Forbid resize of trees in window

--- fusee_window.gd
+++ fusee_window.gd
@@ -5,16 +5,25 @@ const triggers = [1, 4, 7, 10, 14]
 var cut_trees = 0
 
 func _ready() -> void:
-	var player_shadow = $Node2D/PlayerShadow 
+	var player_shadow = $TreesToCut/PlayerShadow 
 	var shadow = 0.5
 	player_shadow.modulate.r = shadow
 	player_shadow.modulate.g = shadow
 	player_shadow.modulate.b = shadow
-
-func _process(delta: float) -> void:
+	
+	# The Simple trees resizes themselves, I put it back to original one because I updated it manually in the UI
+	const scale = 0.3
+	for child in self.get_children():
+		if child is SimpleTree:
+			child.get_node('SpriteAlive1').scale = Vector2(scale, scale)
+	for child in $TreesToCut.get_children():
+		if child is SimpleTree:
+			child.get_node('SpriteAlive1').scale = Vector2(scale, scale)
+			
+func _process(delta: float) -> void: 	
 	var player = get_parent().get_parent().get_parent().get_parent().find_child('Player')
 	var position = player.global_position;
-	var player_shadow = $Node2D/PlayerShadow 
+	var player_shadow = $TreesToCut/PlayerShadow 
 	player_shadow.global_position.x = 0.2 * position.y + 1084
 	var x = player_shadow.global_position.x
 	if (x > 1120):
